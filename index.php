4f1fcc84f58d32266820fdf47394c52274a40abb
ahtec <gerarddoets@gmail.com>
2017-11-18 12:32:22 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Ajax</title>
+    </head>
+    <body>
+        <div id='info'> tis sentance wil be replaced</div>
+        
+        <script>
+        params = "url=amazon.com/gp/aw"
+        request = new ajaxRequest()
+        
+        request.open("POST", "urlpost.php"  , true)
+        request.setRequestHeader("Content-type",application/x-www-form-urlencoded")
+       
+        request.setRequestHeader("Content-lenght",params.length)
+        request.setRequestHeader("Connection","close")
+        
+        
+        
+        
+        
+        </script>
+        
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
